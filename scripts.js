486b729f9db46ae1bcb1c67b41bbcc3b3d7c2a96
Rishabh Singh <ris8645208@gmail.com>
2022-05-04 21:58:33 +0530

    feat: update all the changes

--- scripts.js
+++ scripts.js
@@ -1,4 +1,15 @@
+const arrayUpdate = (menuL) => {
+  let menuListClass = [];
+  for (let i = 0; i < menuL.classList.length; i++) {
+    menuListClass.push(menuL.classList[i]);
+  }
+  return menuListClass;
+};
+
 window.addEventListener("load", function (e) {
+  let menu = document.querySelector(".menub");
+  let close = document.querySelector(".close");
+  let menuL = document.querySelector(".menu");
   document.querySelector(".preloader").style.display = "none";
   var swiper = new Swiper(".myswiper", {
     navigation: {
@@ -9,4 +20,20 @@ window.addEventListener("load", function (e) {
       el: ".swiper-pagination",
     },
   });
+  let menuBtn = document.querySelector("#menuBtn");
+  menuBtn.addEventListener("click", () => {
+    let menuListClass = arrayUpdate(menuL);
+    let body = document.querySelector("body")
+    if(menuListClass.includes("inactive")){
+      menuL.classList.remove("inactive")
+      menuL.classList.add("active")
+      body.style.overflow = "hidden"
+    }
+    else{
+      menuL.classList.remove("active")
+      menuL.classList.add("inactive")
+      body.style.overflow = "auto"
+    }
+    menuListClass = arrayUpdate(menuL);
+  })
 });
